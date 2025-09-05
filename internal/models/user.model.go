8af71a59ba733977482592f045691d823df5c6b3
M16yusuf <yusufsmd58@gmail.com>
2025-09-05 15:13:15 +0700

    feat (user.model)  : add omnitempty for some data struck

--- internal/models/user.model.go
+++ internal/models/user.model.go
@@ -1,14 +1,14 @@
 package models
 
 type User struct {
-	Id         int    `db:"id" json:"user_id"`
-	FirstName  string `db:"first_name" json:"first_name"`
-	LastName   string `db:"last_name" json:"last_name"`
-	AvatarPath string `db:"avatar_path" json:"profile_path"`
-	Email      string `db:"email" json:"email"`
-	Point      int    `db:"point" json:"point"`
-	Role       string `db:"role" json:"role"`
-	Phone      string `db:"phone_number" json:"phone"`
-	Password   string `db:"password" json:"password"`
-	Gender     string `db:"gender" json:"gender"`
+	Id         int     `db:"id" json:"user_id"`
+	FirstName  string  `db:"first_name" json:"first_name"`
+	LastName   string  `db:"last_name" json:"last_name"`
+	AvatarPath string  `db:"avatar_path" json:"profile_path,omitempty"`
+	Email      string  `db:"email" json:"email"`
+	Point      int     `db:"point" json:"point,omitempty"`
+	Role       string  `db:"role" json:"role"`
+	Phone      string  `db:"phone_number" json:"phone,omitempty"`
+	Password   string  `db:"password" json:"password,omitempty"`
+	Gender     *string `db:"gender" json:"gender,omitempty"`
 }
