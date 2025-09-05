553cde29e0290089587801b5b1b72c2cec775cf7
M16yusuf <yusufsmd58@gmail.com>
2025-09-05 20:28:58 +0700

    feat (response.model) : add model for MoviesResponse

--- internal/models/responses.model.go
+++ internal/models/responses.model.go
@@ -10,11 +10,6 @@ type ErrorResponse struct {
 	Err string `json:"error" example:"Error message..."`
 }
 
-type NoRouteResponse struct {
-	ErrorResponse
-	Message string
-}
-
 type TokenResponse struct {
 	Response
 	Token string `json:"token" example:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Miwicm9sZSI6ImFkbWluIiwiZXhwIjoxNzU3MDM4NjQyfQ.J2MAUbAZvFpQl18BkSSyZOSMnbZxPziyZ6q6Bsuj8GU"`
@@ -23,4 +18,10 @@ type TokenResponse struct {
 type ProfileResponse struct {
 	Response
 	Data User
+}
+
+type MoviesResponse struct {
+	Response
+	Data []MovieList
+	Page int `json:"page,omitempty"`
 }
