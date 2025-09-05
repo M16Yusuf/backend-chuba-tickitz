7d65ef3fae590355be65bcca5d3bb6d689aed067
M16yusuf <yusufsmd58@gmail.com>
2025-09-05 13:25:49 +0700

    feat : add response error and responsr with token model

--- internal/models/responses.model.go
+++ internal/models/responses.model.go
@@ -4,3 +4,14 @@ type NoRouteResponse struct {
 	Message string
 	Status  string
 }
+
+type TokenResponse struct {
+	IsSuccess bool   `json:"is_success" example:"true"`
+	Token     string `json:"token" example:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Miwicm9sZSI6ImFkbWluIiwiZXhwIjoxNzU3MDM4NjQyfQ.J2MAUbAZvFpQl18BkSSyZOSMnbZxPziyZ6q6Bsuj8GU"`
+}
+
+type ErrorResponse struct {
+	IsSuccess bool   `json:"is_success" example:"false"`
+	Err       string `json:"error" example:"email atau Password salah"`
+	Code      int    `json:"code,omitempty" example:"400"`
+}
