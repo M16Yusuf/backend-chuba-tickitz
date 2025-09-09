24cf43b4d53bb8b967973c3fd2e992d66970a59e
M16yusuf <yusufsmd58@gmail.com>
2025-09-09 13:55:14 +0700

    refactor(movie.model) : delete omnitempty tag on models.Movie

--- internal/models/movie.model.go
+++ internal/models/movie.model.go
@@ -4,13 +4,13 @@ import "time"
 
 type Movie struct {
 	Id           int        `db:"id" json:"movie_id"`
-	Poster       *string    `db:"poster_path" json:"poster_path,omitempty"`
-	Backdrop     *string    `db:"backdrop_path" json:"backdrop_path,omitempty"`
-	Title        *string    `db:"title" json:"title,omitempty"`
-	Overview     *string    `db:"overview" json:"overview,omitempty"`
-	Release_date *time.Time `db:"release_date" json:"release_date,omitempty"`
-	Duration     *int       `db:"duration" json:"duration,omitempty"`
-	Director_id  *int       `db:"director_id" json:"director_id,omitempty"`
+	Poster       *string    `db:"poster_path" json:"poster_path"`
+	Backdrop     *string    `db:"backdrop_path" json:"backdrop_path"`
+	Title        *string    `db:"title" json:"title"`
+	Overview     *string    `db:"overview" json:"overview"`
+	Release_date *time.Time `db:"release_date" json:"release_date"`
+	Duration     *int       `db:"duration" json:"duration"`
+	Director_id  *int       `db:"director_id" json:"director_id"`
 }
 
 type MovieList struct {
