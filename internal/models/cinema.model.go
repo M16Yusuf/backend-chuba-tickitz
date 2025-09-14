16abe929c849c44367de00eda924160352db6c47
M16yusuf <yusufsmd58@gmail.com>
2025-09-14 08:43:06 +0700

    refactor (model) : add pointer for some model that maybe send null value

--- internal/models/cinema.model.go
+++ internal/models/cinema.model.go
@@ -1,6 +1,6 @@
 package models
 
 type Cinema struct {
-	Id   int    `db:"id" json:"cinema_id"`
-	Name string `db:"name" json:"cinema_name"`
+	Id   *int    `db:"id" json:"cinema_id"`
+	Name *string `db:"name" json:"cinema_name"`
 }
