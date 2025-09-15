6d67065b28f6fd332259dde1b5ab906b6c46e6f6
M16yusuf <yusufsmd58@gmail.com>
2025-09-15 07:24:12 +0700

    feat (migration db) : add seed for some table

---
+++ db/seeds/seed_cinemas.sql
@@ -0,0 +1,4 @@
+insert into cinemas(id,name) values(1,'EBV');
+insert into cinemas(id,name) values(2,'Hiflix');
+insert into cinemas(id,name) values(3,'cineone');
+insert into cinemas(id,name) values(4,'cinepolis');
---
+++ db/seeds/seed_cities.sql
@@ -0,0 +1,5 @@
+insert into cities(id,name) values(1,'Jakarta');
+insert into cities(id,name) values(2,'Bogor');
+insert into cities(id,name) values(3,'Tanggerang');
+insert into cities(id,name) values(4,'Depok');
+insert into cities(id,name) values(5,'Bandung');
