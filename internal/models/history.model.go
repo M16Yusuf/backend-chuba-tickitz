9a860472612e1d632787640931620127e1fe4c03
M16yusuf <yusufsmd58@gmail.com>
2025-09-20 21:42:48 +0700

    refactor : update model for response history

--- internal/models/history.model.go
+++ internal/models/history.model.go
@@ -3,25 +3,23 @@ package models
 import "time"
 
 type Transaction struct {
-	Id         int        `db:"id" json:"transaction_id"`
-	Code       *string    `db:"code_ticket" json:"code_ticket"`
-	MovieID    int        `db:"movies_id" json:"movie_id"`
-	CityID     int        `db:"city_id" json:"city_id"`
-	CinemaID   int        `db:"cinema_id" json:"cinema_id"`
-	ScheduleID int        `db:"schedule_id" json:"schedule_id"`
-	PaymentID  *int       `db:"payment_id" json:"payment_id"`
-	PaidAt     *time.Time `db:"paid_at" json:"paid_at"`
-	Total      float64    `db:"total_price" json:"total"`
-	CreatedAt  time.Time  `db:"created_at" json:"created_at"`
-	Rating     *float64   `db:"rating" json:"rating"`
+	Id        int        `db:"id" json:"transaction_id"`
+	Code      *string    `db:"code_ticket" json:"code_ticket"`
+	PaymentID *int       `db:"payment_id" json:"payment_id"`
+	PaidAt    *time.Time `db:"paid_at" json:"paid_at"`
+	Total     float64    `db:"total_price" json:"total"`
+	CreatedAt time.Time  `db:"created_at" json:"created_at"`
+	Rating    *float64   `db:"rating" json:"rating"`
 }
 
 type History struct {
 	Transaction
-	Movie    Movie
-	Schedule Schedule
-	Payment  Payment
-	Cinema   Cinema
-	City     City
-	Seat     []Seat
+	MovieTitle     string    `json:"movie_title"`
+	ScheduleTime   time.Time `json:"schedule_time"`
+	Payment_method string    `json:"payment_method"`
+	CinemaName     string    `json:"cinema_name"`
+	Cityname       string    `json:"city_name"`
+	Seat           []Seat    `json:"seats"`
 }
+
+// id, code_ticket, paid_at, total_price, created_at, rating
