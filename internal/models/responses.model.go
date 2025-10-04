daeba850aee674ba3fc33306153c45159c30cfd8
M16yusuf <yusufsmd58@gmail.com>
2025-10-04 15:43:23 +0700

    feat (model) : add some model for swagger

--- internal/models/responses.model.go
+++ internal/models/responses.model.go
@@ -69,6 +69,18 @@ type BadRequestResponse struct {
 	Err       string `json:"error" example:"Example bad request error..."`
 }
 
+type UnauthorizedResponse struct {
+	IsSuccess bool   `json:"is_success" example:"false"`
+	Code      int    `json:"code,omitempty" example:"401"`
+	Err       string `json:"error" example:"Unauthorized : please login again..."`
+}
+
+type NotFoundResponse struct {
+	IsSuccess bool   `json:"is_success" example:"false"`
+	Code      int    `json:"code,omitempty" example:"404"`
+	Err       string `json:"error" example:"Not Found"`
+}
+
 type InternalErrorResponse struct {
 	IsSuccess bool   `json:"is_success" example:"false"`
 	Code      int    `json:"code,omitempty" example:"500"`
